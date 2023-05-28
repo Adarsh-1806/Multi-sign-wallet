ee55b820dfd63b7e81928f11ee611dda6bcf9b75
Adarsh-1806 <adarshmoradiya001@gmail.com>
2023-05-28 20:25:58 +0530

    completed basic multi sign contract

---
+++ src/MultiSign.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract MultiSigWallet {
+   event Deposite(address indexed sender, uint256 amount);
+   event TransactionCreated(uint256 indexed ransactionId, address indexed receiver,uint256 amount,bytes data);
+   event TransactionApproved(address indexed signer,uint256 transactionId);
+   event TransactionExecuted(address indexed sender, uint256 amount);
+//    event TransactionConfirmed()
+   address public owner;
+   address[] public signers ;
+   uint8 public required_approvals;
+   mapping(address => bool ) isSigner;
+   mapping(uint256 => mapping(address => bool)) approvals;
+   struct Transaction {
+    address to;
+    uint256 value;
+    bytes data;
+    bool executed;
+    uint8 n_of_confirmations;
+   }
+   
+   uint256 transactionId;
+   mapping(uint256 => Transaction) transactions;
+    
+    modifier onlyOwner(address _owner){
+        require(owner == _owner,"Not Owner");
+        _;
+    }
+    modifier onlyExist(uint256 _id){
+        require(transactionId >= _id,"Invalid Transaction Id");
+        _;
+    }
+    modifier onlySigner(address _sender){
+        require(isSigner[_sender] == true,"Not Authorized to Sign");
+        _;
+    }
+    modifier notExecuted(uint256 _id){
+        require(transactions[_id].executed == false,"Transaction is already Executed");
+        _;
+    }
+
+    constructor(address[] memory _signers,uint8 _required_approvals){
+        require(_signers.length == _required_approvals,"Need to approve Trasaction by all owner");
+        owner = msg.sender;
+        for(uint8 i=0 ;i<_signers.length;i++){
+            isSigner[_signers[i]]=true;
+        }
+        signers = _signers;
+        required_approvals = _required_approvals;
+    }
+
+    function createTransaction(address _to, uint256 _value, bytes memory _data) public{
+        require(_value > 0,"Amount should be more than Zero");
+        transactions[transactionId] = Transaction(
+            _to,
+            _value,
+            _data,
+            false,
+            0
+        );
+        emit TransactionCreated(transactionId, _to, _value, _data);
+    }
+    function approveTransaction(uint256 _id) public onlyExist(_id) notExecuted(_id) onlySigner(msg.sender){
+        
+        Transaction storage tnx = transactions[_id];
+        approvals[_id][msg.sender] = true;
+        tnx.n_of_confirmations += 1;
+
+        emit TransactionApproved(msg.sender,_id);
+    }
+
+    function executeTransaction(uint _id) public onlyExist(_id) notExecuted(_id){
+        Transaction storage tnx = transactions[_id];
+        require(tnx.n_of_confirmations >= required_approvals);
+        (bool success ,) = tnx.to.call{value: tnx.value}(tnx.data);
+
+        require(success,"Transaction Failed");
+        emit TransactionExecuted(msg.sender, tnx.value);
+    }
+
+    function getTransaction(uint256 _id) public view returns(Transaction memory){
+        return transactions[_id];
+    }
+    function addSigner(address[] memory _newSigner) public onlyOwner(msg.sender){
+        uint8 length= uint8(_newSigner.length);
+        required_approvals += length; 
+        for (uint8 i = 0; i < length; i++) {
+            isSigner[_newSigner[i]] = true;   
+            signers.push(_newSigner[i]);   
+        }
+    }
+   receive() payable external {
+    emit Deposite(msg.sender, msg.value);
+   }
+}
